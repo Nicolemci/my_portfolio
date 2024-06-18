e5352397b40c8d0ab2042ef9816926039830ad83
Nicole Mcinerney <nicolemci@hotmail.com>
2024-06-18 12:52:54 +0100

    moved header to application from index page

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@
 
 
 Rails.application.routes.draw do
-  root 'home#index'      # Home page
-  get 'about', to: 'pages#about'   # About page
-  get 'projects', to: 'pages#projects'   # Projects page
-  get 'contact', to: 'pages#contact'   # Contact page
+  root 'home#index'                   # Root URL (home page)
+  get 'about', to: 'pages#about'      # About page
+  get 'projects', to: 'pages#projects'  # Projects page
+  get 'contact', to: 'pages#contact'  # Contact page
 end
